beb3ed41fef425702491798a6e0a222cac406f92
Thomas <thomasszos@icloud.com>
2025-04-08 11:08:06 -0400

    Adjusted Organizing.
    Made TCG Fetch return a completable string.
    added getters and setters.
    Separated classes out of 1 file.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -10,9 +10,12 @@ module org.example.cardcollectorproject {
     requires eu.hansolo.tilesfx;
     requires com.almasb.fxgl.all;
     requires java.net.http;  // Added for HTTP Client API
-    requires com.google.gson; // Added for Gson
+    requires com.google.gson;
+    requires java.smartcardio; // Added for Gson
 
     // Allow reflection into your package for both FXML and Gson if needed
     opens org.example.cardcollectorproject to javafx.fxml, com.google.gson;
     exports org.example.cardcollectorproject;
+    exports org.example.cardcollectorproject.controllers;
+    opens org.example.cardcollectorproject.controllers to com.google.gson, javafx.fxml;
 }
